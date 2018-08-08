bb73c3d8665463501779ec8b80d4441f4a8c5746
yanus <paweljj@gmail.com>
2018-08-08 23:19:32 +0200

    chat scrolling

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -9,9 +9,23 @@ const Chat = ({ message }) => {
 };
 
 class Chats extends Component {
+  constructor(props) {
+    super(props);
+    this.chatsRef = React.createRef();
+  }
+  scrollToBottom = () => {
+    this.chatsRef.current.scrollTop = this.chatsRef.current.scrollHeight;
+  };
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+
   render() {
     return (
-      <div className="Chats">
+      <div className="Chats" ref={this.chatsRef}>
         {this.props.messages.map(message => (
           <Chat message={message} key={message.number} />
         ))}
